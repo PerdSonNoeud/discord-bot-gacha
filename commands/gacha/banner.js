6f8f12c6eca2cf973817f624671870487c5cad55
Christophe <christophejin2004@gmail.com>
2025-09-23 09:24:29 +0200

    Button to change pages of the banners.

--- commands/gacha/banner.js
+++ commands/gacha/banner.js
@@ -1,6 +1,6 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const banners = require('../../banners/banner.js');
-const { setupEmbed } = require('../../config/utility.js');
+const { pagination, setupEmbed } = require('../../config/utility.js');
 
 function bannerEmbed(banner_id, total_banners, user, client) {
   const text = "`- Invocation simple : 10 gemmes 💎`\n`- Invocation multiple : 100 gemmes 💎/🎟️ 1 ticket`"
@@ -21,7 +21,12 @@ module.exports = {
     .setName('banner')
     .setDescription('Affiche les bannières disponibles.'),
   async execute(interaction) {
-    const embed = bannerEmbed(0, 10, interaction.user, interaction.client);
-    await interaction.reply({ embeds: [embed] });
+    const nbBanner = 10;
+    const pages = [];
+    for (let i = 0; i < nbBanner; i++) {
+      pages.push(bannerEmbed(i, nbBanner, interaction.user, interaction.client));
+    }
+
+    pagination(interaction, pages);
   },
 };
--- config/utility.js
+++ config/utility.js
@@ -1,3 +1,5 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
 // Random integer between min and max (inclusive)
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -10,4 +12,77 @@ function setupEmbed(user, client, embed) {
     .setFooter({ text: 'Gacha bot | 2025', iconURL: client.user.displayAvatarURL()})
 }
 
-module.exports = { getRandomInt, setupEmbed };
+
+// Function to setup pagination
+async function pagination(interaction, pages) {
+  let current = 0;
+
+  // Buttons
+  const prev = new ButtonBuilder()
+    .setCustomId('prev')
+    .setLabel('◀️')
+    .setDisabled(true)
+    .setStyle(ButtonStyle.Primary);
+
+  const next = new ButtonBuilder()
+    .setCustomId('next')
+    .setLabel('▶️')
+    .setStyle(ButtonStyle.Primary);
+
+  const border = new ButtonBuilder()
+    .setCustomId('border')
+    .setLabel('⏭️')
+    .setStyle(ButtonStyle.Primary);
+
+  const row = new ActionRowBuilder().addComponents(prev, next, border);
+
+  const message = await interaction.reply({
+    embeds: [pages[current]],
+    components: [row],
+  });
+
+  const collector = message.createMessageComponentCollector({ time: 600000 });
+
+  collector.on('collect', async i => {
+    if (i.user.id !== interaction.user.id) {
+      return i.reply({ content: "These buttons aren't for you!", ephemeral: true });
+    }
+
+    if (i.customId === 'prev') {
+      // Button prev pressed, we go back to the previous page
+      current = current - 1;
+    } else if (i.customId === 'next') {
+      // Button next pressed, we go on to the next page
+      current = current + 1;
+    } else if (i.customId === 'border') {
+      // Button border, if we are on the second half we go to the first page
+      // Otherwise we go to the last page
+      if (current >= Math.floor(pages.length/2)) {
+        current = 0;
+      } else {
+        current = pages.length - 1;
+      }
+    }
+
+    prev.setDisabled(current === 0);
+    next.setDisabled(current === pages.length - 1);
+
+    if (current >= Math.floor(pages.length/2)) {
+      border.setLabel('⏮️');
+    } else {
+      border.setLabel('⏭️');
+    }
+
+    await i.update({ embeds: [pages[current]], components: [row] });
+  });
+
+  collector.on('end', () => {
+    // Disable buttons after time runs out
+    const disabledRow = new ActionRowBuilder().addComponents(
+      row.components.map(button => ButtonBuilder.from(button).setDisabled(true))
+    );
+    message.edit({ components: [disabledRow] });
+  });
+}
+
+module.exports = { getRandomInt, pagination, setupEmbed };
