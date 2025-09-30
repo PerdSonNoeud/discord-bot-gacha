9babce003fd139e773fe4bd06deb30bc5866d039
Christophe <christophejin2004@gmail.com>
2025-09-30 18:34:26 +0200

    Forgot to add this file for the commands (lol).

---
+++ commands/utility/user.js
@@ -0,0 +1,101 @@
+const { MessageFlags, EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { rarityToString } = require('../../characters/character.js');
+const { getPlayer, Player } = require('../../players/player.js');
+const { fileExists } = require('../../config/parser.js');
+const { pagination, setupEmbed } = require('../../config/utility.js');
+
+function playerExists(userID) {
+	return fileExists(`./assets/users/stats/${userID}.json`);
+}
+
+function firstPage(player, user, client) {
+	// TODO: Import data from the inventory of the user
+	const total = 0;
+	const stars5 = 0;
+	const stars4 = 0;
+	const stars3 = total - (stars5 + stars4);
+	let prc5, prc4, prc3;
+
+	if (total === 0) {
+		prc5 = prc4 = prc3 = 0;
+	}
+	else {
+		prc5 = Math.round(stars5 / total * 100, 2);
+		prc4 = Math.round(stars4 / total * 100, 2);
+		prc3 = Math.round(stars3 / total * 100, 2);
+	}
+	const totalText = (
+		`Nombre d'invocations: **${total}**
+    ${rarityToString(5)}: **\`${stars5}\`** \`(${prc5}%)\`
+    ${rarityToString(4)}: **\`${stars4}\`** \`(${prc4}%)\`
+    ${rarityToString(3)}: **\`${stars3}\`** \`(${prc3}%)\``
+	);
+
+	// TODO: Complete uniqueText
+	const uniqueText = '';
+
+	const bagText = (
+		`🪙 - **${player.bag.coins}** pièce${(player.bag.coins > 0) ? 's' : ''}
+    💎 - **${player.bag.gems}** gemme${(player.bag.gems > 0) ? 's' : ''}
+    🎟️ - **${player.bag.tickets}** ticket${(player.bag.tickets > 0) ? 's' : ''}`
+	);
+
+	const embed = new EmbedBuilder()
+		.setTitle('Statistiques')
+		.addFields(
+			{ name: 'Invocations :', value: totalText },
+			{ name: 'Personnages uniques :', value: uniqueText },
+			{ name: 'Inventaire :', value: bagText },
+		);
+	setupEmbed(user, client, embed);
+	return embed;
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Affiche le profile d\'un utilisateur.')
+		.addSubcommand(sub => sub
+			.setName('create')
+			.setDescription('Créer un compte.'),
+		)
+		.addSubcommand(sub => sub
+			.setName('stats')
+			.setDescription('Affiche les statistiques d\'un utilisateur.'),
+		),
+
+	async execute(interaction) {
+		const sub = interaction.options.getSubcommand();
+		let user;
+		if (sub === 'create') {
+			user = interaction.user;
+			if (playerExists) {
+				console.log(`${user.displayName} already has an account.`);
+				await interaction.reply({ content: 'Tu as déjà un compte.', flags: MessageFlags.Ephemeral });
+				return;
+			}
+			const player = Player(user);
+			player.createUser();
+			Player.player.push(player);
+		  await interaction.reply('Compte créé avec succès.');
+		}
+		else if (sub === 'stats') {
+			user = interaction.options.getUser('target');
+			if (!user) {
+			  user = interaction.user;
+			}
+			const pages = [];
+			const current = 0;
+
+			const player = getPlayer(user);
+			pages.push(firstPage(player, user, interaction.client));
+
+			for (let i = 0; i < pages.length; i++) {
+				pages[i].setThumbnail(interaction.guild.iconURL());
+			}
+
+			// TODO: Create the pages for the inventory once added.
+			pagination(interaction, pages, current);
+		}
+	},
+};
